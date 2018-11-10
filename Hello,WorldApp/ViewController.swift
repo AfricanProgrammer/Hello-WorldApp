6d7cf7ad7b3dd75337a9ccf050e0d1a391f2cb7a
NyashaMadzokereAfricanProgrammer <madzokeren@gmail.com>
2018-11-10 22:14:04 +0200

    Initial Commit

---
+++ Hello,WorldApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Hello,WorldApp
+//
+//  Created by user on 11/10/18.
+//  Copyright © 2018 com.africanprogrammer. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
